a44d8e78ca7b25b9c07714a0f794e723f74e2482
faizan <faizanshafiq123@gmail.com>
2021-11-10 14:59:56 -0700

    Faizan Added sqlite db file and make a test for added and displaying data

--- MyNotes/MyNotes/Form1.cs
+++ MyNotes/MyNotes/Form1.cs
@@ -7,14 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SQLite;
 namespace MyNotes
 {
     public partial class Form1 : Form
     {
+        string connectionString = "Data Source=NotesApp.db";
+
+        SQLiteConnection con;
+        SQLiteCommand cmd;
         public Form1()
         {
             InitializeComponent();
+            con = new SQLiteConnection(connectionString, true);
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "SELECT * FROM USER";
+                cmd = new SQLiteCommand(query, con);
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string command = @"INSERT INTO USER (FirstName,LastName,Email,Password) VALUES ('Palak','Sharma','palak@gmail.com','123')";
+            cmd = new SQLiteCommand(command, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
         }
     }
 }
