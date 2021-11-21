d6f4915c9af6ab78b239506ad17aa3fed2583ed2
palaksharma292 <p.sharma649@mybvc.ca>
2021-11-21 01:10:27 -0700

    Created a form and added a localDB
    Connection has been established(testing required)

---
+++ MyNotes/MyNotesApp/MyNotes.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace MyNotesApp
+{
+    partial class MyNotes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MyNotes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "MyNotes";
+            this.Text = "Note";
+            this.Load += new System.EventHandler(this.MyNotes_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ MyNotes/MyNotesApp/MyNotes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyNotesApp
+{
+    public partial class MyNotes : Form
+    {
+        static SqlConnection connection;
+        //static SqlCommand command;
+        //static string query = "";
+        //static SqlCommand command = new SqlCommand(query,connection);
+        //command.ExecuteNonQuery();
+        public MyNotes()
+        {
+            InitializeComponent();
+        }
+
+        private void MyNotes_Load(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(@"Data Source=.\SQLEXPRESS;" +
+                @"AttachDbFilename=|DataDirectory|\NotesDB.mdf;
+                Integrated Security=True;
+                Connect Timeout=30;
+                User Instance=True");
+            connection.Open();
+        }
+    }
+}
