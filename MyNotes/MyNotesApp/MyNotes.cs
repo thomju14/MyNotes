a999940bbee9ce8316efa2a5f138830c632692db
faizan <faizanshafiq123@gmail.com>
2021-11-21 21:26:41 -0700

    Faizan Merged form changes

--- MyNotes/MyNotesApp/MyNotes.cs
+++ MyNotes/MyNotesApp/MyNotes.cs
@@ -13,6 +13,8 @@ namespace MyNotesApp
     public partial class MyNotes : Form
     {
         static SqlConnection connection;
+        string connectionString = "Data Source=NotesApp.db";
+        public int stringCounter { get; set; } = 0;
         //static SqlCommand command;
         //static string query = "";
         //static SqlCommand command = new SqlCommand(query,connection);
@@ -20,6 +22,7 @@ namespace MyNotesApp
         public MyNotes()
         {
             InitializeComponent();
+            pnlNotes.AutoSize = true;
         }
 
         private void MyNotes_Load(object sender, EventArgs e)
@@ -30,6 +33,53 @@ namespace MyNotesApp
                 Connect Timeout=30;
                 User Instance=True");
             connection.Open();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "SELECT * FROM USER";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string command = @"INSERT INTO USER (FirstName,LastName,Email,Password) VALUES ('Palak','Sharma','palak@gmail.com','123')";
+        }
+
+        private void rtbHeading_KeyDown(object sender, KeyEventArgs e)
+        {
+            //stringCounter++;
+            //if (stringCounter > 34)
+            //{
+            //    MessageBox.Show("Sorry Only 34 characters are allowed");
+            //    if (e.KeyCode=>Keys.A || e.)
+            //    return;
+            //}
+        }
+
+        private void rtbHeading_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            char key = e.KeyChar;
+            if (key == '\b')
+                stringCounter--;
+            else
+                stringCounter++;
+
+
+
+            if (rtbHeading.Text.Length >= 34)
+            {
+                MessageBox.Show("Sorry Only 33 characters are allowed");
+                e.Handled = true;
+            }
+
+
         }
     }
 }
